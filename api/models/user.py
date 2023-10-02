9d90c831bb044cf96454b43f21928aa483f4adc4
greatnessmensah <ephraimmensah99@gmail.com>
2023-10-02 18:49:14 +0000

    created post and comment models

--- api/models/user.py
+++ api/models/user.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column, Integer, String, text, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, text, Text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from database.db import Base
 from enum import Enum
+from sqlalchemy.orm import relationship
 
 
 class UserRole(Enum):
@@ -20,4 +21,45 @@ class User(Base):
     password = Column(String, nullable=False)
     role = Column(String, default=UserRole.USER.value)
     created_at = Column(
-        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()"))
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+    posts = relationship("Post", back_populates="owner")
+    comments = relationship("Comment", back_populates="user")
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, nullable=False)
+    content = Column(String, nullable=False)
+    profile_image = Column(String)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    owner = relationship("User", back_populates="posts")
+    comments = relationship("Comment", back_populates="post")
+
+
+class Comment(Base):
+    __tablename__ = "comments"
+    id = Column(Integer, primary_key=True, nullable=False)
+    content = Column(String, nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
+    post = relationship("Post", back_populates="comments")
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    user = relationship("User", back_populates="comments")
+    reply_to_comment_id = Column(Integer, ForeignKey("comments.id"))
+    replies = relationship("Comment", backref="parent_comment", remote_side=[id])
+
+
+class Like(Base):
+    __tablename__ = "likes"
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
