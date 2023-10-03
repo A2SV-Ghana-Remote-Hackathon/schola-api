ffd7f0b2ff8917c06c90f669f15f76f9f0445a06
greatnessmensah <ephraimmensah99@gmail.com>
2023-10-03 09:28:32 +0000

    created events model

--- api/models/user.py
+++ api/models/user.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, ForeignKey, Integer, String, text, Text
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from database.db import Base
 from enum import Enum
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 
 
 class UserRole(Enum):
@@ -41,6 +41,17 @@ class Post(Base):
     comments = relationship("Comment", back_populates="post")
 
 
+class Event(Base):
+    __tablename__ = "events"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    description = Column(Text)
+    event_date = Column(TIMESTAMP(timezone=True), nullable=False)
+    location = Column(String)
+    image = Column(String)  # Event image URL or file path
+    comments = relationship("Comment", back_populates="event", cascade="all, delete-orphan")
+
+
 class Comment(Base):
     __tablename__ = "comments"
     id = Column(Integer, primary_key=True, nullable=False)
@@ -48,12 +59,16 @@ class Comment(Base):
     created_at = Column(
         TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
     )
-    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), nullable=False)
-    post = relationship("Post", back_populates="comments")
+    comment_type = Column(String)  # 'post' or 'event'
+    comment_id = Column(Integer)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"))
+    post = relationship("Post", back_populates="comments", foreign_keys=[comment_id], primaryjoin="and_(Comment.comment_type == 'post', Comment.comment_id == Post.id)")
+    event_id = Column(Integer, ForeignKey("events.id", ondelete="CASCADE"))
+    event = relationship("Event", back_populates="comments", foreign_keys=[comment_id], primaryjoin="and_(Comment.comment_type == 'event', Comment.comment_id == Event.id)")
     user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     user = relationship("User", back_populates="comments")
     reply_to_comment_id = Column(Integer, ForeignKey("comments.id"))
-    replies = relationship("Comment", backref="parent_comment", remote_side=[id])
+    replies = relationship("Comment", backref=backref("parent_comment", remote_side=[id]))
 
 
 class Like(Base):
